55ef1f294c5f74102a14f1fbc8d3b762d8f7916e
Michael Runyon <michaelfrunyon@gmail.com>
2025-05-09 12:24:50 -0700

    Initial Commit

---
+++ HealthAppTestsTests/HealthAppTestsTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthAppTestsTests.swift
+//  HealthAppTestsTests
+//
+//  Created by Michael Runyon on 5/9/25.
+//
+
+import Testing
+
+struct HealthAppTestsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
